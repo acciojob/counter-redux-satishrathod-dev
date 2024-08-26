95830007e83594b7f6616e9a9305d23c9a0d0dfa
Satish Rathod <satishrathod.dev@gmail.com>
2024-08-26 10:05:31 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useContext } from "react";
+import "./../styles/App.css";
+import Counter from "./Counter";
+import { CounterContext } from "../context/CounterContex";
 
 const App = () => {
+  const counterState = useContext(CounterContext);
+
+  console.log(counterState);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Count is {counterState.count}</h1>
+      <Counter />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { CounterProvider } from "./context/CounterContex";
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <CounterProvider>
+    <App />
+  </CounterProvider>,
+  document.getElementById("root")
+);
